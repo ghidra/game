f7c67cdedb94601b5f4672d0d6156d434d8caf0f
ghidra <ghidra@gmail.com>
2014-10-16 23:12:47 -0400

    stage recursion working?

--- lab/stage.js
+++ lab/stage.js
@@ -10,10 +10,6 @@ game.stage.prototype.constructor=game.graph;
 game.stage.prototype.init=function(xdiv,ydiv){
   game.graph.prototype.init.call(this,xdiv,ydiv);
 
-  this.debug=0;
-  this.debug_b=0;
-  this.debug_seed=-1;
-  this.debug_pos_seed=-1;
   this.debug_started_over=0;
 
   this.iterations=0;
@@ -30,6 +26,8 @@ game.stage.prototype.init=function(xdiv,ydiv){
   this.backtracked=[];
 
   this.find_random_path(this.start_position,this.end_position);
+
+  this.geo = this.construct_geo();
 
   return this;
 
@@ -80,8 +78,8 @@ game.stage.prototype.construct_geo=function(){
   s+="<br>iterations:"+this.iterations+"<br>";
   //s+="pos seed:"+this.debug_pos_seed+"<br>";
   //s+="seed:"+this.debug_seed+"<br>";
-  s+="backtracked:"+this.debug+"<br>";
-  s+="clear backtracked:"+this.debug_b+"<br>";
+  //s+="backtracked:"+this.debug+"<br>";
+  //s+="clear backtracked:"+this.debug_b+"<br>";
   s+="started over:"+this.debug_started_over+"<br>";
   return s;
 }
@@ -98,9 +96,6 @@ game.stage.prototype.find_random_path=function(start,end,seed){
   this.next_position(start,end,seed);
 }
 game.stage.prototype.clear_backtrack=function(){
-  if(this.backtracked.length>0){
-    this.debug_b+=1;
-  }
   while(this.backtracked.length > 0) {
     var i = this.backtracked[this.backtracked.length-1];
     this.centers[i].is_room=false;
@@ -111,7 +106,6 @@ game.stage.prototype.clear_backtrack=function(){
   }
 }
 game.stage.prototype.backtrack=function(position,end,seed){//send the backtrack position
-  this.debug+=1;
   if(this.travelled.length<=1){
     this.clear_backtrack();
     this.next_position(this.travelled[this.travelled.length-1],end,seed)
@@ -130,16 +124,12 @@ game.stage.prototype.backtrack=function(position,end,seed){//send the backtrack 
   }
 }
 game.stage.prototype.startover=function(seed){
-  console.log("STARTOVER:"+this.debug_started_over+":"+this.iterations)
+  //console.log("STARTOVER:"+this.debug_started_over+":"+this.iterations)
   this.debug_started_over+=1;
 
   this.clear_backtrack();
   while(this.travelled.length > 0) {
     var p = this.travelled.pop();
-    /*this.centers[p].is_room=false;
-    this.centers[p].visited=false;
-    this.centers[p].connection_direction=-1;
-    this.centers[p].connection_step = -1;*/
   }
   for(var i=0;i<this.centers.length;i++){
     this.centers[i].is_room=false;
@@ -147,16 +137,20 @@ game.stage.prototype.startover=function(seed){
     this.centers[i].connection_direction = -1;
     this.centers[i].connection_step = -1;
   }
-  this.centers[this.end_position].is_room = true;
-  this.centers[this.end_position].visited = true;
+
+  this.start_position=-1;
+  this.end_position=-1;
+  while(this.start_position === this.end_position){
+    this.terminal_positions();
+  }
 
   //console.log(this.travelled)
-  console.log(this.start_position+":"+this.end_position);
-  var n1 = this.centers[this.start_position].neighbor_ids[0];
-  var n2 = this.centers[this.start_position].neighbor_ids[2];
-  var n3 = this.centers[this.start_position].neighbor_ids[4];
-  var n4 = this.centers[this.start_position].neighbor_ids[6];
-  console.log(n1+":"+n2+":"+n3+":"+n4)
+  //console.log(this.start_position+":"+this.end_position);
+  //var n1 = this.centers[this.start_position].neighbor_ids[0];
+  //var n2 = this.centers[this.start_position].neighbor_ids[2];
+  //var n3 = this.centers[this.start_position].neighbor_ids[4];
+  //var n4 = this.centers[this.start_position].neighbor_ids[6];
+  //console.log(n1+":"+n2+":"+n3+":"+n4)
   //console.log(this.centers[n1].visited+":"+this.centers[n2].visited+":"+this.centers[n3].visited+":"+this.centers[n4].visited)
 
   //this.travelled=[];//hold the path finding, where we have travelled
@@ -164,8 +158,6 @@ game.stage.prototype.startover=function(seed){
 
 
   //this.debug=0;
-  //this.debug_b=0;
-  //this.debug_seed=-1;
   //this.debug_pos_seed=-1;
   //this.iterations=0;
   this.catch_recursion=0;
@@ -180,7 +172,6 @@ game.stage.prototype.next_position=function(position,end,seed,search){
   search = search||[0,2,4,6];
   seed+=1;
   this.iterations+=1;
-  if(this.iterations==1)this.debug_seed=Math.asin(seed-1)*(180/Math.PI);
   //console.log("search length:"+search.length);
   var direction_index = Math.floor(this.random(seed)*search.length);//get a random direction
   var this_point = this.centers[position];
@@ -194,6 +185,7 @@ game.stage.prototype.next_position=function(position,end,seed,search){
 
   if(this.catch_recursion>=this.recursion_threshold){
     this.startover(seed);
+    return
   }
 
   if(next === end){//if this equals, we have rached the end
@@ -222,15 +214,15 @@ game.stage.prototype.next_position=function(position,end,seed,search){
       this_point.connection_step = this.travelled.length;
       //this.travelled.push(next);
       this.travelled.push(position);
-      console.log("send:1");
+      //console.log("send:1");
       this.next_position(next,end,seed);//recursion
     }else{
       if(is_exausted || is_trapped){//we need to go back
-        console.log("send:-1:"+is_exausted+":"+is_trapped);
+        //console.log("send:-1:"+is_exausted+":"+is_trapped);
         this.catch_recursion+=1;
         this.backtrack(position,end,seed);//add this point to the back tracked array
       }else{//we need to try again
-        console.log("send:0");
+        //console.log("send:0");
         this.catch_recursion+=1;
         this.next_position(position,end,seed,search);
       }
