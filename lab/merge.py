1ea3a1fe7e2af34f78054e2501aa796557702651
ghidra <ghidra@gmail.com>
2014-11-11 21:00:46 -0500

    ascii and merge script

--- lab/merge.py
+++ lab/merge.py
@@ -1,12 +1,32 @@
+includes=[]
+
 #read in file
-comp_list_file=open("merge_server.txt","r")
-lines=comp_list_file.readlines();
-files=[]
-for line in lines:
-  files.append(line.rstrip("\n"))
+def parse_list(list):
+    comp_list_file=open("merge_server.txt","r")
+    lines=comp_list_file.readlines();
+    files=[]
+    for line in lines:
+        line = line.rstrip("\n")
+        if(line[0]!="#"):#ignore commented out file paths
+            files.append(line.rstrip("\n"))
+    return files;
+
+def print_includes():
+    global includes
+    for f in includes:
+        print f
 
 #now append it all together
-with open("server_merged.js","wb") as outfile:
-  for f in files:
-    with open(f,"rb") as infile:
-      outfile.write(infile.read())
+def merge(filename):
+    global includes
+    with open(filename,"wb") as outfile:
+      for f in includes:
+        with open(f,"rb") as infile:
+          outfile.write(infile.read())
+
+
+
+
+includes = parse_list("merge_server.txt")
+print_includes()
+merge("server_merged.js")
