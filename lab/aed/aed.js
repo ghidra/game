dcbe896c4ebfda84d85e405ba9af30d727d10dd1
ghidra <ghidra@gmail.com>
2016-05-05 18:03:59 -0400

    small gui improvements

--- lab/aed/aed.js
+++ lab/aed/aed.js
@@ -10,13 +10,12 @@ aed.palette_canvas={};
 aed.palette_canvas_settings={};
 aed.palette_colors={};
 aed.palette_parameters={};
+aed.console={};
 //aed.palette_colors_custom={};
 
 aed.palette_large = new aed.graph_symbols("large",aed.size);
 aed.colors = new aed.graph_colors("colorgraph",aed.size);
 aed.colors_custom = new aed.graph_colors_custom("customcolorgraph",aed.size);
-
-aed.graph_controls={};//hold the graph control elements
 
 aed.file = new rad.io("aed");//method to save and load
 
@@ -68,9 +67,10 @@ aed.load_file=function(file){
     }
     aed.frames[g].merge( convert );
     //aed.frames[g].merge( file[g] );
-    aed.palette_canvas.innerHTML="";
+    //aed.palette_canvas.innerHTML="";
   }
-  aed.palette_canvas.appendChild(aed.frames[0].render());
+  //aed.palette_canvas.appendChild(aed.frames[0].render());
+  set_canvas_size(file[0].xdiv);
 }
 aed.make_io_window=function(){
   //make the window to import and export from
@@ -94,166 +94,23 @@ aed.import_graph=function(){
 
 function init(){
 
-  aed.graph_controls.canvassize = new rad.dropdown({
-    "id":"graphsize",
-    "label":"graph size",
-    "style":{
-      "clear":"none",
-      "float":"left",
-      "height":"auto"
-    },
-    "style_label":{"width":0},
-    "options":{
-      0:"4x4",
-      1:"8x8",
-      2:"16x16",
-      3:"32x32"
-    },
-    "value": "3",
-    "callback":function(arg){
-      //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
-      set_canvas_size(Math.pow(2,Number(arg.getvalue())+2));
-      //console.log(Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+1);
-    }
-  });
-
-  aed.graph_controls.numframes = new rad.textbox({
-    "id":"numberofframe",
-    "label":"frames",
-    "value": "1",
-    "style":{
-      "clear":"none",
-      "float":"left",
-      "width":40,
-      "height":"auto"
-    },
-    "style_textbox":{
-      "width":40
-    },
-    "style_label":{
-      "width":0
-    },
-    "callback":function(arg){
-      //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
-      add_frames(arg.getvalue());
-    }
-  });
-  aed.graph_controls.frameslider = new rad.slider({
-    "id":"frameslider",
-    "label":"frame",
-    "value": "1",
-    "fontsize":10,
-    "settings":{
-      "clamped":true,
-      "upper":2,
-      "lower":1,
-      "max_upper":2,
-      "max_lower":1,
-      "int":true,
-      "update":true
-    },
-    "style":{
-      "clear":"none",
-      "float":"left",
-      "height":"auto"
-    },
-    "style_label":{
-      "width":0
-    },
-    "callback":function(arg){
-      //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
-      //console.log(arg.getvalue());
-      change_frame(arg.getvalue());
-    }
-  });
-
-  ///------save and load
-  var saveas_tb = new rad.textbox({
-    "id":"saveas",
-    "label":"save as",
-    "value":"",
-    "style":{"width":280,"clear":"left","float":"left"},
-    "style_label":{"width":140},
-    "style_textbox":{"width":140}
-  });
-  var saveas_bu = new rad.button({
-    "id":"saveas_bu",
-    "label":"save",
-    "style":{"width":40,"float":"left","clear":"none"},
-    "callback":function(arg){
-      //get the save file name
-      var filename = document.getElementById("tb_saveas_save as").value;
-      if(filename===""){
-        alert("save: no file name given");
-        return null;
-      }
-      //aed.sanitize_for_save(aed.frames);
-      aed.file.save(filename,aed.frames,aed.sanitize_for_save);
-      //now I can refresh the load drop box
-    }
-  });
-  
-  var file_list = aed.file.list();
-  if(!file_list){//no files found,make the arrays we need
-    file_list = ["no files found"];
-  }
-
-  var load_dd = new rad.dropdown({
-    "id":"load",
-    "label":"load",
-    "options":file_list,
-    "value":0,
-    "style":{"width":280,"clear":"left","float":"left"},
-    "style_label":{"width":140},
-    "style_dropdown":{"width":140}
-  });
-  var load_bu = new rad.button({
-    "id":"bu_load",
-    "label":"load",
-    "style":{"width":40,"float":"left","clear":"none"},
-    "callback":function(arg){
-      var fileid = document.getElementById("dd_load_load").value;
-      var filename = file_list[fileid];
-      var loadedfile = aed.file.load(filename);
-      if (loadedfile != 'none'){
-        aed.load_file(loadedfile);//load the file
-      }else{
-        alert(filename+' file not found');
-      }
-    }
-  });
-
-  var export_bu = new rad.button({
-    "id":"bu_export",
-    "label":"export",
-    "callback":function(arg){
-      aed.export_graph();//get the graph as string, and put it in the floating window
-    }
-  });
-
-  var import_bu = new rad.button({
-    "id":"bu_import",
-    "label":"import",
-    "callback":function(arg){
-      aed.import_graph();
-    }
-  });
-
   var layout = {
     'split':0,
     'size':90,
+    'style':{'letterSpacing':2},
     'partitions':{
       'container_main':{
         'split':1,
         'size':50,
         'partitions':{
-          'container_canvas':{
-            'split':0,
+          'canvas':{
+          //'container_canvas':{
+           /* 'split':0,
             'size':70,
             'partitions':{
               'canvas':{},
               'canvas_settings':{}
-            }
+            }*/
           },
           'container_palettes':{
             'split':1,
@@ -274,7 +131,9 @@ function init(){
           }
         }
       },
-      'console':{}
+      'console':{
+        'style':{'fontSize':8}
+      }
     }
   };
   aed.panels = new rad.panels("layout",layout,rad.closure(aed,aed.windowresized));//,rad.closure(this,this.windowresized)
@@ -282,10 +141,11 @@ function init(){
   //get all the panels to store for later
   aed.palette_symbols  = aed.panels.get_panel("symbols");
   aed.palette_canvas  = aed.panels.get_panel("canvas");
-  aed.palette_canvas_settings = aed.panels.get_panel("canvas_settings");
+  //aed.palette_canvas_settings = aed.panels.get_panel("canvas_settings");
   aed.palette_colors  = aed.panels.get_panel("colors");
   aed.palette_parameters = aed.panels.get_panel("parameters");
   //aed.palette_colors_custom = aed.panels.get_panel("custom_colors");
+  aed.console = aed.panels.get_panel("console");
   
   //set the palettes and colors up
   aed.palette_large.fetch_ascii(13312);//1305
@@ -301,34 +161,40 @@ function init(){
 
   aed.palette_parameters.innerHTML="";//add in the radio box for paint mode
 
-  //canvas settings
-  aed.palette_canvas_settings.innerHTML="";
-  aed.palette_canvas_settings.appendChild( aed.graph_controls.canvassize.getelement() );
-  aed.palette_canvas_settings.appendChild( aed.graph_controls.numframes.getelement()) ;
-  aed.palette_canvas_settings.appendChild( aed.graph_controls.frameslider.getelement() );
-  //save and load
-  aed.palette_canvas_settings.appendChild( saveas_tb.getelement()) ;
-  aed.palette_canvas_settings.appendChild( saveas_bu.getelement() );
-  aed.palette_canvas_settings.appendChild( load_dd.getelement()) ;
-  aed.palette_canvas_settings.appendChild( load_bu.getelement() );
-  aed.palette_canvas_settings.appendChild( import_bu.getelement()) ;
-  aed.palette_canvas_settings.appendChild( export_bu.getelement() );
-
   //now add in the canvas
   set_canvas_size();
+   //canvas settings
+  //aed.palette_canvas.innerHTML="";
+  //just put something in the console, so that I know its there
+  aed.console.innerHTML="initalized";
 }
 
 function set_canvas_size( s ){
-  s = s||32;
+  if(s==undefined){
+    s = 32;
+    aed.frames[0] = new aed.graph_canvas("canvasgraph",aed.size,s,s);
+    aed.frames[0].set_symbols_graph(aed.palette_large);
+  }
   aed.graph_size=s;
   //get the number of frames
-  var frames = aed.graph_controls.numframes.getvalue();
-  for(var i=0; i<frames;i++){
-    aed.frames[i] = new aed.graph_canvas("canvasgraph",aed.size,s,s);
-    aed.frames[i].set_symbols_graph(aed.palette_large);
-    aed.palette_canvas.innerHTML="";
-    aed.palette_canvas.appendChild(aed.frames[i].render());
-  }
+  //var frames = 1;//aed.graph_controls.numframes.getvalue();
+  //for(var i=0; i<frames;i++){
+  //  aed.frames[i] = new aed.graph_canvas("canvasgraph",aed.size,s,s);
+  //  aed.frames[i].set_symbols_graph(aed.palette_large);
+  //}
+  aed.palette_canvas.innerHTML="";
+  aed.palette_canvas.appendChild(aed.frames[0].render());
+  ///controls
+  aed.palette_canvas.appendChild( aed.graph_controls.canvassize.getelement() );
+  aed.palette_canvas.appendChild( aed.graph_controls.numframes.getelement()) ;
+  aed.palette_canvas.appendChild( aed.graph_controls.frameslider.getelement() );
+  //save and load
+  aed.palette_canvas.appendChild( aed.graph_controls.saveas_tb.getelement()) ;
+  aed.palette_canvas.appendChild( aed.graph_controls.saveas_bu.getelement() );
+  aed.palette_canvas.appendChild( aed.graph_controls.load_dd.getelement()) ;
+  aed.palette_canvas.appendChild( aed.graph_controls.load_bu.getelement() );
+  aed.palette_canvas.appendChild( aed.graph_controls.import_bu.getelement()) ;
+  aed.palette_canvas.appendChild( aed.graph_controls.export_bu.getelement() );
 }
 function add_frames(n){
   var nf = n-aed.frames.length;
@@ -347,11 +213,162 @@ function add_frames(n){
   aed.graph_controls.frameslider.refresh();
 }
 function change_frame(f){
+  //HERE WE NEED TO DO SOME PREPENDING TO THE CANVAS LAYER
+  //insertBefore(child, parent.firstchild)
   //console.log(aed.frames);
   aed.palette_canvas.innerHTML="";
   aed.palette_canvas.appendChild(aed.frames[rad.clamp(Math.round(f)-1,0,aed.frames.length-1)].render());
 }
 
+
+//GUI CRAP
+aed.graph_controls={};
+
+aed.graph_controls.canvassize = new rad.dropdown({
+  "id":"graphsize",
+  "label":"graph size",
+  "style":{
+    "clear":"left",
+    "float":"left",
+    "height":"auto"
+  },
+  "style_label":{"width":0},
+  "options":{
+    0:"4x4",
+    1:"8x8",
+    2:"16x16",
+    3:"32x32"
+  },
+  "value": "3",
+  "callback":function(arg){
+    //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
+    set_canvas_size(Math.pow(2,Number(arg.getvalue())+2));
+    //console.log(Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+1);
+  }
+});
+
+aed.graph_controls.numframes = new rad.textbox({
+  "id":"numberofframe",
+  "label":"frames",
+  "value": "1",
+  "style":{
+    "clear":"none",
+    "float":"left",
+    "width":40,
+    "height":"auto"
+  },
+  "style_textbox":{
+    "width":40
+  },
+  "style_label":{
+    "width":0
+  },
+  "callback":function(arg){
+    //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
+    add_frames(arg.getvalue());
+  }
+});
+aed.graph_controls.frameslider = new rad.slider({
+  "id":"frameslider",
+  "label":"frame",
+  "value": "1",
+  "fontsize":10,
+  "settings":{
+    "clamped":true,
+    "upper":2,
+    "lower":1,
+    "max_upper":2,
+    "max_lower":1,
+    "int":true,
+    "update":true
+  },
+  "style":{
+    "clear":"none",
+    "float":"left",
+    "height":"auto"
+  },
+  "style_label":{
+    "width":0
+  },
+  "callback":function(arg){
+    //set_canvas_size(Math.pow(2,Number(document.getElementById("dd_"+arg.id+"_"+arg.label).value)+2));
+    //console.log(arg.getvalue());
+    change_frame(arg.getvalue());
+  }
+});
+///------save and load
+aed.graph_controls.saveas_tb = new rad.textbox({
+  "id":"saveas",
+  "label":"save as",
+  "value":"",
+  "style":{"width":280,"clear":"left","float":"left"},
+  "style_label":{"width":140},
+  "style_textbox":{"width":140}
+});
+aed.graph_controls.saveas_bu = new rad.button({
+  "id":"saveas_bu",
+  "label":"save",
+  "style":{"width":40,"float":"left","clear":"none"},
+  "callback":function(arg){
+    //get the save file name
+    var filename = document.getElementById("tb_saveas_save as").value;
+    if(filename===""){
+      alert("save: no file name given");
+      return null;
+    }
+    //aed.sanitize_for_save(aed.frames);
+    aed.file.save(filename,aed.frames,aed.sanitize_for_save);
+    //now I can refresh the load drop box
+  }
+});
+
+aed.graph_controls.file_list = aed.file.list();
+if(!aed.graph_controls.file_list){//no files found,make the arrays we need
+  aed.graph_controls.file_list = ["no files found"];
+}
+
+aed.graph_controls.load_dd = new rad.dropdown({
+  "id":"load",
+  "label":"load",
+  "options":aed.graph_controls.file_list,
+  "value":0,
+  "style":{"width":280,"clear":"left","float":"left"},
+  "style_label":{"width":140},
+  "style_dropdown":{"width":140}
+});
+aed.graph_controls.load_bu = new rad.button({
+  "id":"bu_load",
+  "label":"load",
+  "style":{"width":40,"float":"left","clear":"none"},
+  "callback":function(arg){
+    var fileid = document.getElementById("dd_load_load").value;
+    var filename = aed.graph_controls.file_list[fileid];
+    var loadedfile = aed.file.load(filename);
+    if (loadedfile != 'none'){
+      aed.load_file(loadedfile);//load the file
+    }else{
+      alert(filename+' file not found');
+    }
+  }
+});
+
+aed.graph_controls.export_bu = new rad.button({
+  "id":"bu_export",
+  "label":"export",
+  "callback":function(arg){
+    aed.export_graph();//get the graph as string, and put it in the floating window
+  }
+});
+
+aed.graph_controls.import_bu = new rad.button({
+  "id":"bu_import",
+  "label":"import",
+  "callback":function(arg){
+    aed.import_graph();
+  }
+});
+//----
+//----
 window.onload=function(){
     init();
 }
